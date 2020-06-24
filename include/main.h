adbbb45229aee9734e1abfa6375028629f5bf702
Mirko Covizzi <mrkcvzz@gmail.com>
2020-06-24 21:43:45 +0200

    Implement interrupts and timer LED blinking

--- include/main.h
+++ include/main.h
@@ -2,6 +2,25 @@
 
 typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+
+// Cortex-M3 uses only array registers 0-2
+// Alignment can be found at page 128 of Cortex-M3 Programming Manual
+typedef struct {
+    __IO uint32_t ISER[8U];
+    uint32_t RESERVED0[24U];
+    __IO uint32_t ICER[8U];
+    uint32_t RSERVED1[24U];
+    __IO uint32_t ISPR[8U];
+    uint32_t RESERVED2[24U];
+    __IO uint32_t ICPR[8U];
+    uint32_t RESERVED3[24U];
+    __IO uint32_t IABR[8U];
+    uint32_t RESERVED4[56U];
+    __IO uint8_t IP[240U];
+    uint32_t RESERVED5[644U];
+    __IO uint32_t STIR;
+} NVIC_Type;
 
 typedef struct {
     __IO uint32_t CR;
@@ -28,59 +47,77 @@ typedef struct {
     __IO uint32_t LCKR;
 } GPIO_TypeDef;
 
-typedef struct
-{
+typedef struct {
     __IO uint16_t CR1;
-    uint16_t  RESERVED0;
+    uint16_t RESERVED0;
     __IO uint16_t CR2;
-    uint16_t  RESERVED1;
+    uint16_t RESERVED1;
     __IO uint16_t SMCR;
-    uint16_t  RESERVED2;
+    uint16_t RESERVED2;
     __IO uint16_t DIER;
-    uint16_t  RESERVED3;
+    uint16_t RESERVED3;
     __IO uint16_t SR;
-    uint16_t  RESERVED4;
+    uint16_t RESERVED4;
     __IO uint16_t EGR;
-    uint16_t  RESERVED5;
+    uint16_t RESERVED5;
     __IO uint16_t CCMR1;
-    uint16_t  RESERVED6;
+    uint16_t RESERVED6;
     __IO uint16_t CCMR2;
-    uint16_t  RESERVED7;
+    uint16_t RESERVED7;
     __IO uint16_t CCER;
-    uint16_t  RESERVED8;
+    uint16_t RESERVED8;
     __IO uint16_t CNT;
-    uint16_t  RESERVED9;
+    uint16_t RESERVED9;
     __IO uint16_t PSC;
-    uint16_t  RESERVED10;
+    uint16_t RESERVED10;
     __IO uint16_t ARR;
-    uint16_t  RESERVED11;
+    uint16_t RESERVED11;
     __IO uint16_t RCR;
-    uint16_t  RESERVED12;
+    uint16_t RESERVED12;
     __IO uint16_t CCR1;
-    uint16_t  RESERVED13;
+    uint16_t RESERVED13;
     __IO uint16_t CCR2;
-    uint16_t  RESERVED14;
+    uint16_t RESERVED14;
     __IO uint16_t CCR3;
-    uint16_t  RESERVED15;
+    uint16_t RESERVED15;
     __IO uint16_t CCR4;
-    uint16_t  RESERVED16;
+    uint16_t RESERVED16;
     __IO uint16_t BDTR;
-    uint16_t  RESERVED17;
+    uint16_t RESERVED17;
     __IO uint16_t DCR;
-    uint16_t  RESERVED18;
+    uint16_t RESERVED18;
     __IO uint16_t DMAR;
-    uint16_t  RESERVED19;
+    uint16_t RESERVED19;
 } TIM_TypeDef;
+
+// Memory mapping can be found at page 128 of Cortex-M3 Programming Manual
+#define NVIC ((NVIC_Type *) 0xE000E100)
 
 #define RCC ((RCC_TypeDef *) 0x40021000)
 
 #define GPIOB ((GPIO_TypeDef *) 0x40010C00)
+#define GPIOC ((GPIO_TypeDef *) 0x40011000)
 
 #define TIM3EN ((uint32_t) 1 << 1)
+#define TIM4EN ((uint32_t) 1 << 2)
+
 #define IOPBEN ((uint32_t) 1 << 3)
+#define IOPCEN ((uint32_t) 1 << 4)
 
 #define GPIO_CRL_MODE1 ((uint32_t) 1 << 5 | 1 << 4)
 #define GPIO_CRL_CNF1_0 ((uint32_t) 1 << 6)
 #define GPIO_CRL_CNF1_1 ((uint32_t) 1 << 7)
 
+#define GPIO_CRH_MODE13_0 ((uint32_t) 1 << 20)
+#define GPIO_CRH_CNF13 ((uint32_t) 1 << 23 | 1 << 22)
+#define ODR13 ((uint32_t) 1 << 13)
+
 #define TIM3 ((TIM_TypeDef *) 0x40000400)
+#define TIM4 ((TIM_TypeDef *) 0x40000800)
+
+#define TIM_CR1_ARPE ((uint16_t) 1 << 7)
+#define TIM_CR1_CEN ((uint16_t) 1)
+#define TIM_DIER_UIE ((uint16_t) 1)
+#define TIM_SR_UIF ((uint16_t) 1)
+
+
--- src/main.c
+++ src/main.c
@@ -1,5 +1,35 @@
 #include "main.h"
 
+void TIM4_IRQHandler(void) {
+    // Check status bit
+    if (TIM4->SR & TIM_SR_UIF) {
+        // Clear status bit
+        TIM4->SR &= ~TIM_SR_UIF;
+        GPIOC->ODR ^= ODR13;
+    }
+}
+
+void enable_led_timer(void) {
+    // Activate LED
+    RCC->APB2ENR |= IOPCEN;
+    GPIOC->CRH &= ~GPIO_CRH_CNF13;
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;
+    GPIOC->ODR ^= ODR13;
+
+    // Enable TIMER4 clock
+    RCC->APB1ENR |= TIM4EN;
+    TIM4->PSC = 36000;
+    TIM4->ARR = 250;
+    TIM4->CCR1 = 2;
+    TIM4->DIER = TIM_DIER_UIE;
+    // Enable Auto Preload
+    TIM4->CR1 |= TIM_CR1_ARPE;
+    // Enable TIMER4 Interrupt
+    NVIC->ISER[0] = (uint32_t) 1 << 30;
+    // Enable timer counter
+    TIM4->CR1 |= TIM_CR1_CEN;
+}
+
 void enable_pwm(void) {
     // Enable GPIOB clock
     RCC->APB2ENR |= IOPBEN;
@@ -15,8 +45,10 @@ void enable_pwm(void) {
     GPIOB->CRL |= GPIO_CRL_MODE1;
 
     TIM3->ARR = 65535;
-    TIM3->PSC = 16;
-    TIM3->CCR4 = 32;
+    // Change to modulate rate
+    TIM3->PSC = 32;
+    // Change to modulate intensity
+    TIM3->CCR4 = 4096;
 
     // PWM Mode 1
     TIM3->CCMR2 |= (1 << 14 | 1 << 13);
@@ -30,13 +62,14 @@ void enable_pwm(void) {
     TIM3->CCMR2 |= 1 << 11;
 
     // Enable Auto Preload
-    TIM3->CR1 |= 1 << 7;
+    TIM3->CR1 |= TIM_CR1_ARPE;
     // Enable timer counter
-    TIM3->CR1 |= 1;
+    TIM3->CR1 |= TIM_CR1_CEN;
 }
 
 int main(void)
 {
+    enable_led_timer();
     enable_pwm();
 
     while (1) {}
